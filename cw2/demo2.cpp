dc5734290f85cd43d4984903b59de8863ad931d6
Meteor <93meteor@gmail.com>
2016-11-19 20:55:26 +0000

    cw2

--- cw2/demo2.cpp
+++ cw2/demo2.cpp
@@ -1,7 +1,15 @@
 #include "demo2.h"
 #include "Ray.h"
 #include "Object.h"
-#define DEPTH1 3
+
+//What I implement
+//The DEPTH1 is that a ray from the eye through all objects in the scene, up to
+//a recursion depth of at least 3.
+
+//The INFINITY1 is used to initialize the nearest intersection distance, when
+//comparing the intersection of multiple objects at the same time which one is
+//nearer
+#define DEPTHE1 3
 #define INFINITY1 1e8
 
 //window resolution (default 640x480)
@@ -15,6 +23,8 @@ glm::mat4 viewMatrix; //The transformation matrix for the virtual camera
 //An object in the scene
 Object* object = NULL;
 
+//What I implement
+//Define a triangel, a plane and two sphere in the scene
 glm::vec3 a(10.0f,10.0f,0.0f);
 glm::vec3 b(60.0f,60.0f,0.0f);
 glm::vec3 c(120.0f,120.0f,0.0f);
@@ -26,9 +36,13 @@ glm::vec3 n(0.0f, 1.0f, 1.0f);
 Plane p(p0, n);
 
 
-glm::vec3 center(-10.0f, 40.0f, 50.0f);
+glm::vec3 center(-200.0f, 40.0f, 50.0f);
 float r = 50.0;
 Sphere s(center, r);
+
+glm::vec3 center(200.0f, 40.0f, 50.0f);
+float r = 30.0;
+Sphere s2(center, r);
 
 //Perform any cleanup of resources here
 void cleanup()
@@ -42,13 +56,17 @@ void cleanup()
 bool CheckIntersection(const Ray &ray, IntersectInfo &info);
 float CastRay(Ray &ray, Payload &payload);
 
+
 //Function for testing for intersection with all the objects in the scene
-//If an object is hit then info contains the information on the intersection, 
+//If an object is hit then info contains the information on the intersection,
 //returns true if an object is hit, false otherwise
+
+//What I implement
 bool CheckIntersection(const Ray &ray, IntersectInfo &info)
 {
 	// Test here for intersection of ray with nearest object
 	float tNear = INFINITY1;
+	// Used to record the nearest object's information of intersection
 	IntersectInfo near_info;
 	bool if_intersect = false;
 	// if(p.Intersect(ray, info) && info.time < tNear) {
@@ -56,12 +74,17 @@ bool CheckIntersection(const Ray &ray, IntersectInfo &info)
 	// 	tNear = near_info.time;
 	// 	if_intersect = true;
 	// }
-	if(t.Intersect(ray, info) && info.time < tNear) {
+	// if(t.Intersect(ray, info) && info.time < tNear) {
+	// 	near_info = info;
+	// 	tNear = near_info.time;
+	// 	if_intersect = true;
+	// }
+	if(s.Intersect(ray, info) && info.time < tNear) {
 		near_info = info;
 		tNear = near_info.time;
 		if_intersect = true;
 	}
-	if(s.Intersect(ray, info) && info.time < tNear) {
+	if(s2.Intersect(ray, info) && info.time < tNear) {
 		near_info = info;
 		tNear = near_info.time;
 		if_intersect = true;
@@ -80,16 +103,17 @@ bool CheckIntersection(const Ray &ray, IntersectInfo &info)
 //@payload Information on the current ray i.e. the cumulative color and the number of bounces it has performed
 //returns either the time of intersection with an object (the coefficient t in the equation: RayPosition = RayOrigin + t*RayDirection) or zero to indicate no intersection
 
+//What I implement
 float CastRay(Ray &ray, Payload &payload)
 {
 	//Perform early termination here (use number of bounces)
 	//Check if the ray intersects something
-	
+
 	if(payload.numBounces == DEPTH1)
 		return 0.0f;
 	else{
 		IntersectInfo info;
-		if(CheckIntersection(ray,info)){		
+		if(CheckIntersection(ray,info)){
 			glm::vec3 lightPos(-60.0f, 90.0f, 100.0f);
 			// lightPos = glm:normalize(lightPos);
 			glm::vec3 norm = info.normal;
@@ -109,9 +133,9 @@ float CastRay(Ray &ray, Payload &payload)
 			float d = abs(light_inten * diff * cos_theta);
 			// float sp = abs(light_inten * spec * pow(cos_alpha, spec_inten));
 			// float sp;
-			
+
 			float sp = light_inten * spec * pow(max(glm::dot(norm, h),0.0f), spec_inten);
-			
+
 			float a = abs(light_inten * ambient);
 		//  The values of cos_theta and cos_alpha should be set to 0 if they are negative
 			payload.numBounces ++;
@@ -120,7 +144,7 @@ float CastRay(Ray &ray, Payload &payload)
 		else
 			return 0.0f;
 	}
-	
+
 }
 
 /*--- Display Function ---*/
@@ -142,7 +166,7 @@ void DemoDisplay()
 	float fov = 90.0f;
 	//Value for adjusting the pixel position to account for the field of view
 	float fovAdjust = tan(fov*0.5f *(M_PI/180.0f));
-	
+
 	//Tell OpenGL to start rendering points
 	glBegin(GL_POINTS);
 	//Iterate over each pixel in the image
@@ -157,7 +181,7 @@ void DemoDisplay()
 			float pixelScreenX = 2.0f*pixelNormX - 1.0f;
 			float pixelScreenY = 1.0f-2.0f*pixelNormY;
 
-			//Account for Field of View			
+			//Account for Field of View
 			float pixelCameraX = pixelScreenX * fovAdjust;
 			float pixelCameraY = pixelScreenY * fovAdjust;
 
@@ -177,10 +201,10 @@ void DemoDisplay()
 			//Set up ray in world space
 			Ray ray(glm::vec3(rayOrigin), //The origin of the ray we are casting
 				glm::normalize(glm::vec3(pixelCameraSpace - rayOrigin))//The direction the ray is travelling in
-			);	
+			);
 
 			//Structure for storing the information we get from casting the ray
-			Payload payload;			
+			Payload payload;
 
 			//Default color is white
 			glm::vec3 color(1.0f);
@@ -189,7 +213,7 @@ void DemoDisplay()
 			//Cast our ray into the scene
 			if(CastRay(ray,payload) > 0.0f){// > 0.0f indicates an intersection
 				color = ray.direction;
-			}	
+			}
 			//Get OpenGL to render the pixel with the color from the ray
 			glColor3f(color.x,color.y,color.z);
 			glVertex3f(pixelScreenX,pixelScreenY,0.0f);
@@ -228,12 +252,12 @@ int main(int argc, char **argv)
 	//initialise OpenGL
 	glutInit(&argc, argv);
 	//Define the window size with the size specifed at the top of this file
-	glutInitWindowSize(windowX, windowY);	
+	glutInitWindowSize(windowX, windowY);
 
 	//Create the window for drawing with the title "SimpleExample"
-	glutCreateWindow("CG-CW2");	
+	glutCreateWindow("CG-CW2");
 
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);	
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
 	//Set the function demoDisplay (defined above) as the function that
 	//is called when the window must display
